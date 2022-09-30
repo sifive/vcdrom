097a2369924494b304d5117990f89321a807a580
Kevin Mills <kevin.mills@sifive.com>
2022-09-30 11:00:09 -0700

    fix circular

--- ceg/wsClient.js
+++ ceg/wsClient.js
@@ -3,21 +3,6 @@ let processMessage = require('./processMessage.js');
 
 let websocket;
 
-let wsClientInitialize = () => {
-  websocket = new WebSocket('ws://' + location.host);
-  websocket.onopen = () => {
-    console.log('websocket opened wow :)' + websocket.url);
-    websocket.send('iam ceg');
-    // websocket.send(msg1);
-  };
-  websocket.onmessage = (msg) => {
-    // on getting a message from FS
-    console.log('Message from web socket server :) ): : ', msg.data.toString());
-    // return the message
-    processMessage.processMessages(msg.data.toString());
-  };
-};
-
 function sendMessage(msg1) {
   // Wait until the state of the socket is not ready and send the message when it is...
   waitForSocketConnection(websocket, function () {
@@ -40,6 +25,21 @@ function waitForSocketConnection(socket, callback) {
     }
   }, 5); // wait 5 milisecond for the connection...
 }
+
+let wsClientInitialize = () => {
+  websocket = new WebSocket('ws://' + location.host);
+  websocket.onopen = () => {
+    console.log('websocket opened wow :)' + websocket.url);
+    websocket.send('iam ceg');
+    // websocket.send(msg1);
+  };
+  websocket.onmessage = (msg) => {
+    // on getting a message from FS
+    console.log('Message from web socket server :) ): : ', msg.data.toString());
+    // return the message
+    processMessage.processMessages(msg.data.toString());
+  };
+};
 
 module.exports = { wsClientInitialize, sendMessage };
 
